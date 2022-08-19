6cf5319169ddabb47f0c478da35fc7e6ea9a79a8
Iswaryacodes <111576351+Iswaryacodes@users.noreply.github.com>
2022-08-19 05:33:09 -0700

    Add files via upload

---
+++ Assignments.js
@@ -0,0 +1,48 @@
+// Assignments on 18th Aug,2022
+/*
+//Assignment no 1
+// Description: To find the given year is leap year or not
+let a = 1804;
+function leapyear(a)      //function statement
+{
+    if(a % 4 === 0 && a % 100 != 0 || a % 400 === 0)    
+    console.log(`The year ${a} is a Leap year`);
+else
+    console.log(`The year ${a} is not a Leap year`);
+}
+leapyear(a);       //leapyear function calling
+*/
+
+
+/*
+//Assignment no 2
+// Description: Conversion of temperature to and from celsius and fahrenheit 
+//  formula used as given: [c/5=(f-32)/9]
+let c = 60;  
+let f = 45;  
+let d = "\u00B0";    // degree symbol code
+function ctof(c)    // function statement for converting celsius to fahrenheit
+{
+    let f = (9*c/5)+32;
+    console.log(`${c}${d}C equals ${f}${d}F`);
+}
+ctof(c);        //function calling for converting celsius to fahrenheit
+
+function ftoc(f)     // function statement for converting fahrenheit to celsius
+{
+    let c = (f-32)*5/9;
+    console.log(`${f}${d}F equals ${c}${d}C`);
+}
+ftoc(f);    //function calling for converting fahrenheit to celsius
+*/
+
+
+/*
+//Assignment no 3
+// Description: To find factorial of a number
+let n = 1;
+let fact = 1;
+for(i=1;i<=n;i++)
+    fact *= i;     //factorial formula
+console.log(`The Value of ${n}! is ${fact}`);
+*/
