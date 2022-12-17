31fce120f2efaa76910cfda7956a0ba780f0e672
Iswaryacodes <111576351+Iswaryacodes@users.noreply.github.com>
2022-12-17 14:47:59 +0530

    Add files via upload

---
+++ Assignment_14.js
@@ -0,0 +1,73 @@
+//////Q no1:
+/*let employees = [
+    {
+    "id": 1,
+    "name":"Abhinav",
+    "department":"IT",
+    "Salary":75000
+    },
+    {
+    "id": 2,
+    "name":"Gaurav",
+    "department":"Sales",
+    "Salary":52000
+    },
+    {
+    "id": 3,
+    "name":"Raj",
+    "department":"IT",
+    "Salary":64000
+    }];
+
+//// a)filter employees according to department = IT
+let DepartmentFilter = employees.filter((employees) =>  employees.department =='IT');
+console.log(DepartmentFilter);
+
+
+//////b) filter employees who earn a salary < 65000
+let SalaryFilter = employees.filter((employees) =>  employees.Salary < 65000);
+console.log(SalaryFilter);
+*/
+
+//////Qno:2
+/////a)a) Number of Beds > 200
+
+/*
+let hospitals = [
+    {
+    "id": 1,
+    "name":"Hospital A",
+    "location":"Delhi",
+    "noOfBeds": 450,
+    "availability":"Yes"
+    },
+    {
+    "id": 2,
+    "name":"Hospital B",
+    
+    "location":"Pune",
+    "noOfBeds": 150,
+    "availability":"No"
+    },
+    {
+    "id": 3,
+    "name":"Hospital C",
+    "location":"Pune",
+    "noOfBeds": 350,
+    "availability":"Yes"
+}];
+
+////a) Number of Beds > 200
+
+let Bedsbelow200 = hospitals.filter(hospitals => hospitals.noOfBeds >200);
+console.log(Bedsbelow200);
+
+//////b) Availability of Beds = Yes
+let AvailableBeds = hospitals.filter(hospitals => hospitals.availability =="Yes");
+console.log(AvailableBeds);
+
+
+/////c) Location = Pune
+let PuneLocation = hospitals.filter(hospitals => hospitals.location =="Pune");
+console.log(PuneLocation);
+*/
